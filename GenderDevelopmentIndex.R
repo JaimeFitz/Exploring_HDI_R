cbd3454b9343a78c558b05e1c66b837a115118e9
Jaime Fitzgerald <106002818+JaimeFitz@users.noreply.github.com>
2022-05-31 18:17:14 -0400

    Update GenderDevelopmentIndex.R

--- GenderDevelopmentIndex.R
+++ GenderDevelopmentIndex.R
@@ -31,12 +31,58 @@ summary(HDI_data)
 
 ggplot(data=HDI_data, aes(x=ExpYearsEduMale19, y=ExpYearsEduFem19)) +
       geom_point()+
-	geom_smooth(method=lm, se=FALSE)
+	geom_smooth(method=lm, se=FALSE, color = "purple")+
+labs(title="Comparison of Education by Gender",
+       x="Expected Years of Education \n (Females)", y = "Expected Years of Education \n (Females)")+
+theme_classic()
 
 #Plotting female years of education against male years of education
 
+ggplot(data = HDI_data, aes(x = EstGNIPerCapMale19, y = EstGNIPerCapFem19)) + 
+geom_point()+
+geom_smooth(method=lm, se=FALSE, color = "red")+
+labs(title="Comparison of GNI by Gender",
+       x="Estimated GNI \n Per Capita (Females)", y = "Estimated GNI \n Per Capita (Females)")+
+theme_classic()
+
+#Plotting estimated GNI for females against estimated GNI for males
+
+ggplot(data=HDI_data, aes(x=LifeExpFem19, y=ExpYearsEduFem19)) +
+      geom_point()+
+	geom_smooth(method=lm, se=FALSE, color = "green")+
+labs(title="Comparison of Education and Life Expectancy (Females)",
+       x="Life Expectancy \n (Females)", y = "Expected Years of Education \n (Females)")+
+theme_classic()
+
+#Plotting life expectancy and education for females
+
+ggplot(data=HDI_data, aes(x=LifeExpMale19, y=ExpYearsEduMale19)) +
+      geom_point()+
+	geom_smooth(method=lm, se=FALSE, color = "yellow")+
+labs(title= "Comparison of Education and Life Expectancy (Males)",
+       x="Life Expectancy \n (Males)", y = "Expected Years of Education \n (Males)")+
+theme_classic()
+
+#Plotting life expectancy and education for males
+
+ggplot(data=HDI_data, aes(x=LifeExpFem19, y=ExpYearsEduFem19)) +
+      geom_point()+
+	geom_smooth(method=lm, se=FALSE, color = "blue")+
+labs(title= "Comparison of Gross National Income and Life Expectancy (Females)",
+       x="Life Expectancy \n (Females)", y = "Estimated Gross National Income \n (Females)")+
+theme_classic()
+
+#Plotting female estimated GNI against female life expectancy
+
+ggplot(data=HDI_data, aes(x=LifeExpMale19, y= EstGNIPerCapMale19)) +
+      geom_point()+
+	geom_smooth(method=lm, se=FALSE, color = "orange")+
+labs(title= "Comparison of Gross National Income and Life Expectancy (Males)",
+       x="Life Expectancy \n (Males)", y = "Estimated Gross National Income \n (Males)")+
+theme_classic()
 
 
+#Plotting male estimated GNI against male life expectancy
 #######################Basic graphics#########################
 
 # Clear console

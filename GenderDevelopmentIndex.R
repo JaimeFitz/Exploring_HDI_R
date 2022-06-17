5ccc8831aaff11d33206bf234a7722307a3af65c
Jaime Fitzgerald <106002818+JaimeFitz@users.noreply.github.com>
2022-06-17 19:22:38 -0400

    Update GenderDevelopmentIndex.R

--- GenderDevelopmentIndex.R
+++ GenderDevelopmentIndex.R
@@ -33,7 +33,7 @@ ggplot(data=HDI_data, aes(x=ExpYearsEduMale19, y=ExpYearsEduFem19)) +
       geom_point()+
 	geom_smooth(method=lm, se=FALSE, color = "purple")+
 labs(title="Comparison of Education by Gender",
-       x="Expected Years of Education \n (Females)", y = "Expected Years of Education \n (Females)")+
+       x="Expected Years of Education \n (Males)", y = "Expected Years of Education \n (Females)")+
 theme_classic()
 
 #Plotting female years of education against male years of education
@@ -65,25 +65,47 @@ theme_classic()
 
 #Plotting life expectancy and education for males
 
-ggplot(data=HDI_data, aes(x=LifeExpFem19, y=ExpYearsEduFem19)) +
+ggplot(data=HDI_data, aes(x=ExpYearsEduFem19, y=EstGNIPerCapFem19)) +
       geom_point()+
 	geom_smooth(method=lm, se=FALSE, color = "blue")+
 labs(title= "Comparison of Gross National Income and Life Expectancy (Females)",
-       x="Life Expectancy \n (Females)", y = "Estimated Gross National Income \n (Females)")+
+       x="Expected Years of Education \n (Females)", y = "Estimated Gross National Income \n (Females)")+
 theme_classic()
 
-#Plotting female estimated GNI against female life expectancy
+#Plotting female estimated GNI against expected years of education
 
-ggplot(data=HDI_data, aes(x=LifeExpMale19, y= EstGNIPerCapMale19)) +
+ggplot(data=HDI_data, aes(x=ExpYearsEduMale19, y= EstGNIPerCapMale19)) +
       geom_point()+
 	geom_smooth(method=lm, se=FALSE, color = "orange")+
 labs(title= "Comparison of Gross National Income and Life Expectancy (Males)",
-       x="Life Expectancy \n (Males)", y = "Estimated Gross National Income \n (Males)")+
+       x="Expected Years of Education \n (Males)", y = "Estimated Gross National Income \n (Males)")+
 theme_classic()
 
 
-#Plotting male estimated GNI against male life expectancy
+#Plotting male estimated GNI against expected years of education
+
 #######################Basic graphics#########################
+
+cor.test(HDI_data$ExpYearsEduMale19, HDI_data$ExpYearsEduFem19, method = "pearson")
+#Testing the correlation between male expected years of education and female expected years of education
+#cor.test will return the correlation coefficient as well as the p-value (significance)
+
+
+cor.test(HDI_data$EstGNIPerCapMale19, HDI_data$EstGNIPerCapFem19, method = "pearson")
+#Testing the correlation between male estimated GNI per capita and female estimated GNI per capita
+
+cor.test(HDI_data$LifeExpFem19, HDI_data$ExpYearsEduFem19, method = "pearson")
+#Testing the correlation between female life expectancy and expected years of education
+
+cor.test(HDI_data$ExpYearsEduMale19, HDI_data$ExpYearsEduFem19, method = "pearson")
+
+
+cor.test(HDI_data$ExpYearsEduMale19, HDI_data$ExpYearsEduFem19, method = "pearson")
+
+
+
+
+###########Calculating Correlations###########################
 
 # Clear console
 cat("\014") 
